7a297bdf2bf6055ba8d7438426762e002e184199
Shruthi <ashruthi2006@gmail.com>
2025-09-06 22:21:16 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,4 @@
+a=int(input("Enter A :"))
+b=int(input("Enter B :"))
+if(a>b): print("A is big")
+else : print("B is big")
---
+++ fruits.py
@@ -0,0 +1,27 @@
+fruit=input("enter the fruits do you want:").strip().lower()
+if fruit=='oranges':
+    print(f"{fruit} per kg 50RS")
+elif fruit=='apples':
+    print(f"{fruit} per kg 120RS")
+elif fruit=='bananas':
+    print(f"{fruit} per dozen 40RS")
+elif fruit=='cherries':
+     print(f"{fruit} per kg 600RS")
+else:
+     print("sorry ! the fruit is out of stack")
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
